4a5daad6f233953b965cfbb5dbb351015a39a313
Marco <marcosilva@criticalmanufacturing.com>
2021-07-29 18:34:32 +0100

    feat(new command): add reset switch for templating engine (useful for upgrades)

--- cmf-cli/Commands/new/NewCommand.cs
+++ cmf-cli/Commands/new/NewCommand.cs
@@ -1,4 +1,7 @@
+using System;
 using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO.Abstractions;
 using Cmf.Common.Cli.Attributes;
 
 namespace Cmf.Common.Cli.Commands
@@ -7,13 +10,51 @@ namespace Cmf.Common.Cli.Commands
     /// "new" command group
     /// </summary>
     [CmfCommand("new")]
-    public class NewCommand : BaseCommand
+    public class NewCommand : TemplateCommand
     {
+        private Command _cmd;
         /// <summary>
         /// Configure command
         /// </summary>
         /// <param name="cmd"></param>
         public override void Configure(Command cmd)
+        {
+            this._cmd = cmd;
+            cmd.AddOption(new Option<bool>(
+                aliases: new[] { "--reset" },
+                description: "Reset template engine. Use this if after an upgrade the templates are not working correctly."
+            ));
+            cmd.Handler = CommandHandler.Create<bool>(Execute);
+        }
+
+        /// <summary>
+        /// Execute command
+        /// </summary>
+        /// <param name="reset"></param>
+        public void Execute(bool reset)
+        {
+            if (reset)
+            {
+                RunCommand(new []{ "--debug:reinit" });
+            }
+            else
+            {
+                this._cmd.Invoke(new [] { "-h" });
+            }
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public NewCommand() : base("new")
+        {
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="fileSystem"></param>
+        public NewCommand(IFileSystem fileSystem) : base("new", fileSystem)
         {
         }
     }
