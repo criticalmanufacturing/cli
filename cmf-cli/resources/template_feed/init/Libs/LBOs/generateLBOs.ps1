39210a37a6cb9408f43bdfb564eb7d502bab93b9
Marco <marcosilva@criticalmanufacturing.com>
2021-08-04 12:06:07 +0100

    fix(init): lbo script had unreplaced tokens

--- cmf-cli/resources/template_feed/init/Libs/LBOs/generateLBOs.ps1
+++ cmf-cli/resources/template_feed/init/Libs/LBOs/generateLBOs.ps1
@@ -3,13 +3,13 @@ $pwd = Split-Path $invocation.MyCommand.Path
 $lbosPath = "$PSScriptRoot\..\LBOs"
 
 # Create a new session remotely to wait for process
-$sess = New-PSSession -ComputerName "<%= $PLASTER_PARAM_vmHostname %>"
+$sess = New-PSSession -ComputerName "<%= $CLI_PARAM_vmHostname %>"
 Enter-PSSession -Session $sess
 Write-Host "Deal with host config file"
 
-Invoke-Command -Session $sess -Scriptblock {$protectUnprotectConfigFilePath = '<%= $PLASTER_PARAM_InstallationPath %>\ProtectUnprotectConfigFile'}
-Invoke-Command -Session $sess -Scriptblock {$lboGeneratorPath = '<%= $PLASTER_PARAM_InstallationPath %>\LBOGenerator'}
-Invoke-Command -Session $sess -Scriptblock {$pathBiz = '<%= $PLASTER_PARAM_InstallationPath %>\BusinessTier'}
+Invoke-Command -Session $sess -Scriptblock {$protectUnprotectConfigFilePath = '<%= $CLI_PARAM_InstallationPath %>\ProtectUnprotectConfigFile'}
+Invoke-Command -Session $sess -Scriptblock {$lboGeneratorPath = '<%= $CLI_PARAM_InstallationPath %>\LBOGenerator'}
+Invoke-Command -Session $sess -Scriptblock {$pathBiz = '<%= $CLI_PARAM_InstallationPath %>\BusinessTier'}
 Invoke-Command -Session $sess -Scriptblock {$arguments = "/Mode:2 /InstallPath:""$pathBiz"""}
 Invoke-Command -Session $sess -Scriptblock {Set-Location -Path $protectUnprotectConfigFilePath}
 Invoke-Command -Session $sess -Scriptblock {Start-Process Cmf.Tools.ProtectUnprotectConfigFile.exe $arguments -Wait}
@@ -27,8 +27,8 @@ if((Test-Path "$lbosPath\TypeScript" -PathType Container)) {
     Remove-Item "$lbosPath\TypeScript" -Recurse -Force
 }
 
-$SOURCE = "<%= $PLASTER_PARAM_InstallationPath %>"
-$installation = '\\<%= $PLASTER_PARAM_vmHostname %>\' + $SOURCE -replace ':', '$'
+$SOURCE = "<%= $CLI_PARAM_InstallationPath %>"
+$installation = '\\<%= $CLI_PARAM_vmHostname %>\' + $SOURCE -replace ':', '$'
 Remove-Item -Path $pwd\* -Recurse -Force -Exclude "generateLBOs.ps1"
 Copy-Item "$installation\LBOGenerator\out\NetStandard" -Destination "$lbosPath" -Recurse -Force -Filter '*.dll'
 Copy-Item "$installation\LBOGenerator\out\TypeScript" -Destination "$lbosPath" -Recurse -Force
