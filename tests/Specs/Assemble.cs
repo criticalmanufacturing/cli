af068113c40897bfcb21ab43b86a23f97fe9265b
João Pereira <joaopereira@criticalmanufacturing.com>
2021-12-09 15:21:47 +0000

    chore(assemble): assemble should ignore CriticalManufacturing and Cmf.Environment dependencies #84

--- tests/Specs/Assemble.cs
+++ tests/Specs/Assemble.cs
@@ -232,5 +232,50 @@ namespace tests.Specs
 
             Assert.IsTrue(assembledTestFiles.Contains($"{packageTest.Key}.{packageTest.Value}.zip"));
         }
+
+        [TestMethod]
+        public void Assemble_WithDefaultDependenciesToIgnore()
+        {
+            string cirepo = @"/cirepo";
+            KeyValuePair<string, string> packageRoot = new("Cmf.Custom.Package", "1.1.0");
+            KeyValuePair<string, string> packageDep1 = new("CriticalManufacturing", "8.3.0");            
+            KeyValuePair<string, MockDirectoryData> assembleOutputDir = new("/test/assemble/", new());
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/test/cmfpackage.json", new MockFileData(
+                @$"{{
+                  ""packageId"": ""{packageRoot.Key}"",
+                  ""version"": ""{packageRoot.Value}"",
+                  ""description"": ""This package deploys Critical Manufacturing Customization"",
+                  ""packageType"": ""Root"",
+                  ""isInstallable"": true,
+                  ""isUniqueInstall"": false,
+                  ""dependencies"": [
+                    {{
+                         ""id"": ""{packageDep1.Key}"",
+                        ""version"": ""{packageDep1.Value}""
+                    }}
+                  ]
+                }}")},
+                { @$"{cirepo}/{packageRoot.Key}.{packageRoot.Value}.zip", new DFPackageBuilder().CreateManifest(packageRoot.Key, packageRoot.Value, new Dictionary<string, string>() { { packageDep1.Key, packageDep1.Value}  }).ToMockFileData() },
+                { @$"{cirepo}/{packageDep1.Key}.{packageDep1.Value}.zip", new DFPackageBuilder().CreateManifest(packageDep1.Key, packageDep1.Value).ToMockFileData() },
+            });
+
+            var assembleCommand = new AssembleCommand(fileSystem);
+
+            assembleCommand.Execute(fileSystem.DirectoryInfo.FromDirectoryName("test"), fileSystem.DirectoryInfo.FromDirectoryName(assembleOutputDir.Key), new UriBuilder() { Scheme = Uri.UriSchemeFile, Host = "", Path = cirepo }.Uri, null, false);
+
+            IEnumerable<string> assembledFiles = fileSystem.DirectoryInfo.FromDirectoryName(assembleOutputDir.Key).EnumerateFiles("*.zip").Select(f => f.Name);
+            Assert.AreEqual(1, assembledFiles.Count());
+
+            Assert.IsTrue(assembledFiles.Contains($"{packageRoot.Key}.{packageRoot.Value}.zip"));
+            Assert.IsFalse(assembledFiles.Contains($"{packageDep1.Key}.{packageDep1.Value}.zip"));
+
+            IFileInfo dependenciesJsonFile = fileSystem.DirectoryInfo.FromDirectoryName(assembleOutputDir.Key).EnumerateFiles(CliConstants.FileDependencies).FirstOrDefault();
+            Assert.IsNotNull(dependenciesJsonFile);
+            Assert.IsTrue(dependenciesJsonFile.Exists);
+            Assert.AreEqual("{}", dependenciesJsonFile.OpenText().ReadToEnd());
+        }
     }
 }
