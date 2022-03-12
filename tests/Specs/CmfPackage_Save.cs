8fdd2e56d2c84a656a2e987e4063267f0fa23ff1
Ricardo Filipe Pereira <RicardoPereira@criticalmanufacturing.com>
2022-03-12 18:00:56 +0000

    test: added tests for removal of JsonIgnore from property IsMandatory in Dependency object

--- tests/Specs/CmfPackage_Save.cs
+++ tests/Specs/CmfPackage_Save.cs
@@ -174,5 +174,279 @@ namespace tests.Specs
             var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
             Assert.True(cmfpackageFileContent.Contains("handlerVersion"), "Package.json should have handler version");
         }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is not kept during serialization if value is true
+        /// </summary>
+        [TestMethod]
+        public void DoNotKeepIsMandatoryPropInDependencyIfValueIsTrue()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*"",
+                            ""mandatory"": ""true""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsFalse(cmfpackageFileContent.Contains("mandatory"), "Package.json should not have ismandatory in dependency");
+        }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is not added during serialization
+        /// </summary>
+        [TestMethod]
+        public void DoNotAddIsMandatoryPropInDependency()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsFalse(cmfpackageFileContent.Contains("mandatory"), "Package.json should not have ismandatory in dependency");
+        }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is kept during serialization if value is false
+        /// </summary>
+        [TestMethod]
+        public void KeepIsMandatoryPropInDependencyIfValueIsFalse()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*"",
+                            ""mandatory"": ""false""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsTrue(cmfpackageFileContent.Contains("mandatory"), "Package.json should have ismandatory in dependency");
+        }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is not kept during serialization if value is true
+        /// </summary>
+        [TestMethod]
+        public void DoNotKeepIsMandatoryPropInDependencyIfValueIsTrue()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*"",
+                            ""mandatory"": ""true""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsFalse(cmfpackageFileContent.Contains("mandatory"), "Package.json should not have ismandatory in dependency");
+        }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is not added during serialization
+        /// </summary>
+        [TestMethod]
+        public void DoNotAddIsMandatoryPropInDependency()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsFalse(cmfpackageFileContent.Contains("mandatory"), "Package.json should not have ismandatory in dependency");
+        }
+
+        /// <summary>
+        /// Validates that the isMandatory property in Dependency is kept during serialization if value is false
+        /// </summary>
+        [TestMethod]
+        public void KeepIsMandatoryPropInDependencyIfValueIsFalse()
+        {
+            KeyValuePair<string, string> package = new("Cmf.Custom.Package", "1.1.0");
+
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { "/repo/cmfpackage.json", new MockFileData(
+                @$"{{
+                      ""packageId"": ""{ package.Key }"",
+                      ""version"": ""{ package.Value }"",
+                      ""description"": ""Custom Package"",
+                      ""packageType"": ""Generic"",
+                      ""isInstallable"": true,
+                      ""isUniqueInstall"": true,
+                      ""dependencies"": [
+                        {{
+                            ""id"": ""CriticalManufacturing.DeploymentMetadata"",
+                            ""version"": ""8.3.*"",
+                            ""mandatory"": ""false""
+                        }}
+                      ],
+                      ""contentToPack"": [
+                        {{
+                            ""source"": ""*"",
+                            ""target"": """"
+                        }}
+                        ],
+                      ""handlerVersion"": 2
+                }}")}
+            });
+
+            IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"repo/{CliConstants.CmfPackageFileName}");
+            CmfPackage cmfPackageObj = CmfPackage.Load(cmfpackageFile, fileSystem: fileSystem);
+
+            Assert.IsNotNull(cmfPackageObj.HandlerVersion);
+
+            cmfPackageObj.SaveCmfPackage();
+
+            var cmfpackageFileContent = fileSystem.File.ReadAllText(cmfpackageFile.FullName);
+            Assert.IsTrue(cmfpackageFileContent.Contains("mandatory"), "Package.json should have ismandatory in dependency");
+        }
     }
 }
