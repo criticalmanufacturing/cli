0b1982adadcb2d1c7cbe708c0b54d0d1a066eb87
João Pereira <joaopereira@criticalmanufacturing.com>
2022-10-25 15:18:25 +0100

    test: support BuildablePackages on data packages

--- tests/Specs/Build.cs
+++ tests/Specs/Build.cs
@@ -1,4 +1,6 @@
 using Cmf.CLI.Builders;
+using Cmf.CLI.Commands;
+using Cmf.CLI.Commands.restore;
 using Cmf.CLI.Constants;
 using Cmf.CLI.Core.Constants;
 using Cmf.CLI.Core.Objects;
@@ -6,11 +8,13 @@ using Cmf.CLI.Core.Utilities;
 using Cmf.CLI.Factories;
 using Cmf.CLI.Handlers;
 using Cmf.CLI.Interfaces;
+using Cmf.CLI.Utilities;
 using Cmf.Common.Cli.TestUtilities;
 using FluentAssertions;
 using Moq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Spectre.Console.Rendering;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -458,5 +462,194 @@ public class Build
         StringWriter standardOutput = (new Logging()).GetLogStringWriter();
         businessPackageTypeHandler.Build(false);
         Assert.DoesNotContain("Executing 'Run Business Unit Tests'", standardOutput.ToString().Trim());
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void DataBuild_WithBuildablePackages(bool withBuildableSteps)
+    {
+        KeyValuePair<string, string> packageRoot = new("Cmf.Custom.Package", "1.1.0");
+        KeyValuePair<string, string> packageData = new("Cmf.Custom.Data", "1.1.0");
+
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            // project config file
+            { ".project-config.json", new MockFileData("")},
+
+            // root cmfpackage file
+            { $"cmfpackage.json", new MockFileData(
+            @$"{{
+                ""packageId"": ""{packageRoot.Key}"",
+                ""version"": ""{packageRoot.Value}"",
+                ""description"": ""This package deploys Critical Manufacturing Customization"",
+                ""packageType"": ""Root"",
+                ""isInstallable"": true,
+                ""isUniqueInstall"": false
+            }}")}
+        });
+
+        if (!withBuildableSteps)
+        {
+            // data cmfpackage file
+            fileSystem.AddFile($"{packageData.Key}/{CliConstants.CmfPackageFileName}", new MockFileData(
+            @$"{{
+                ""packageId"": ""{packageData.Key}"",
+                ""version"": ""{packageData.Value}"",
+                ""description"": ""This package deploys Critical Manufacturing Customization"",
+                ""packageType"": ""Data"",
+                ""isInstallable"": true,
+                ""isUniqueInstall"": true,
+                ""contentToPack"": [
+                {{
+                    ""source"": ""DEEs/*"",
+                    ""target"": ""DeeRules"",
+                    ""contentType"": ""DEE""
+                }}
+                ]
+            }}"));
+        }
+        else
+        {
+            // data cmfpackage file
+            fileSystem.AddFile($"{packageData.Key}/{CliConstants.CmfPackageFileName}", new MockFileData(
+            @$"{{
+                    ""packageId"": ""{packageData.Key}"",
+                    ""version"": ""{packageData.Value}"",
+                    ""description"": ""This package deploys Critical Manufacturing Customization"",
+                    ""packageType"": ""Data"",
+                    ""isInstallable"": true,
+                    ""isUniqueInstall"": true,
+                    ""contentToPack"": [
+                    {{
+                        ""source"": ""DEEs/*"",
+                        ""target"": ""DeeRules"",
+                        ""contentType"": ""DEE""
+                    }}
+                    ],
+                    ""buildablePackages"": [
+                        ""../Cmf.Custom.Business""
+                    ]
+                }}"));
+
+            // business cmfpackage file
+            fileSystem.AddFile("Cmf.Custom.Business/cmfpackage.json", new MockFileData(
+            @$"{{
+              ""packageId"": ""Cmf.Custom.Business"",
+              ""version"": ""1.1.0"",
+              ""description"": ""Cmf Custom Business Package"",
+              ""packageType"": ""Business"",
+              ""isInstallable"": true,
+              ""isUniqueInstall"": false,
+              ""contentToPack"": [
+                {{
+                  ""source"": ""Release/*.dll"",
+                  ""target"": """",
+                  ""ignoreFiles"": [
+                    "".cmfpackageignore""
+                  ]
+                }}
+              ]
+            }}"));
+
+            // business sln
+            fileSystem.AddFile("Cmf.Custom.Business/Business.sln", new MockFileData(
+            @$"Microsoft Visual Studio Solution File, Format Version 12.00
+                # Visual Studio Version 17
+                VisualStudioVersion = 17.3.32825.248
+                MinimumVisualStudioVersion = 10.0.40219.1
+                Project(""{{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}}"") = ""Cmf.Custom.Common"", ""Cmf.Custom.Common\Cmf.Custom.Common.csproj"", ""{{6C3D698D-7F9E-4E39-A571-DEB63582CA52}}""
+                Project(""{{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}}"") = ""Cmf.Custom.Actions"", ""..\Data\DEEs\Cmf.Custom.Actions.csproj"", ""{{22BF79FE-7D2A-4F61-A115-DAB07DE05686}}""
+                EndProject
+                Global
+	                GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		                Debug|Any CPU = Debug|Any CPU
+		                Release|Any CPU = Release|Any CPU
+	                EndGlobalSection
+	                GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		                {{6C3D698D-7F9E-4E39-A571-DEB63582CA52}}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		                {{6C3D698D-7F9E-4E39-A571-DEB63582CA52}}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		                {{6C3D698D-7F9E-4E39-A571-DEB63582CA52}}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		                {{6C3D698D-7F9E-4E39-A571-DEB63582CA52}}.Release|Any CPU.Build.0 = Release|Any CPU
+		                {{22BF79FE-7D2A-4F61-A115-DAB07DE05686}}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		                {{22BF79FE-7D2A-4F61-A115-DAB07DE05686}}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		                {{22BF79FE-7D2A-4F61-A115-DAB07DE05686}}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		                {{22BF79FE-7D2A-4F61-A115-DAB07DE05686}}.Release|Any CPU.Build.0 = Release|Any CPU
+	                EndGlobalSection
+	                GlobalSection(SolutionProperties) = preSolution
+		                HideSolutionNode = FALSE
+	                EndGlobalSection
+	                GlobalSection(ExtensibilityGlobals) = postSolution
+		                SolutionGuid = {{B6998FE2-5739-49CF-939B-73DB4B5DAF2E}}
+	                EndGlobalSection
+                EndGlobal"));
+
+            // common csproj
+            fileSystem.AddFile("Cmf.Custom.Business/Cmf.Custom.Common/Cmf.Custom.Common.csproj", new MockFileData(
+            @$"<Project Sdk=""Microsoft.NET.Sdk"">
+                      <PropertyGroup>
+                        <TargetFramework>net6.0</TargetFramework>
+                        <ImplicitUsings>enable</ImplicitUsings>
+                        <Nullable>enable</Nullable>
+                      </PropertyGroup>
+                </Project>"));
+
+            // class file
+            fileSystem.AddFile("Cmf.Custom.Business/Cmf.Custom.Common/TestClass.cs", new MockFileData(
+            @$"namespace Cmf.Custom.Actions
+                {{
+                    public class Class1
+                    {{
+                    }}
+                }}"));
+
+            // deeeactions csproj
+            fileSystem.AddFile("Cmf.Custom.Data/DEEs/Cmf.Custom.Actions.csproj", new MockFileData(
+            @$"<Project Sdk=""Microsoft.NET.Sdk"">
+                      <PropertyGroup>
+                        <TargetFramework>net6.0</TargetFramework>
+                        <ImplicitUsings>enable</ImplicitUsings>
+                        <Nullable>enable</Nullable>
+                      </PropertyGroup>
+                </Project>"));
+
+            // deeActionfile
+            fileSystem.AddFile("Cmf.Custom.Data/DEEs/TestDEE.cs", new MockFileData(
+            @$"namespace Cmf.Custom.Actions
+            {{
+                public class Class1
+                {{
+                }}
+            }}"));
+        }
+
+        ExecutionContext.Initialize(fileSystem);
+
+        IFileInfo cmfpackageFile = fileSystem.FileInfo.FromFileName($"Cmf.Custom.Data/{CliConstants.CmfPackageFileName}");
+        DataPackageTypeHandlerV2 packageTypeHandler = PackageTypeFactory.GetPackageTypeHandler(cmfpackageFile) as DataPackageTypeHandlerV2;
+
+        packageTypeHandler.BuildablePackagesHandlers
+            .HasAny()
+            .Should().Be(withBuildableSteps);
+
+        packageTypeHandler.BuildablePackagesHandlers
+            .Any(BuildablePackageHandler =>
+            {
+                bool result = false;
+                if (BuildablePackageHandler is BusinessPackageTypeHandler)
+                {
+                    var businessPackageTypeHandler = BuildablePackageHandler as BusinessPackageTypeHandler;
+                    if (businessPackageTypeHandler.BuildSteps[0] is ExecuteCommand<RestoreCommand>)
+                    {
+                        var buildStep = (businessPackageTypeHandler.BuildSteps[0] as ExecuteCommand<RestoreCommand>);
+                        if (buildStep.Command is RestoreCommand)
+                        {
+                            result = true;
+                        }
+                    }
+                }
+                return result;
+            })
+            .Should().Be(withBuildableSteps);
     }
 }
