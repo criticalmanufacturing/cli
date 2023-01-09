7325679ef1eaed3dea47b12ba309cc7267497552
João Pereira <joaopereira@criticalmanufacturing.com>
2023-01-09 11:03:47 +0000

    chore: add Startup Module that can be used in other .NET plugins

--- cmf-cli/Objects/VersionService.cs
+++ cmf-cli/Objects/VersionService.cs
@@ -6,9 +6,9 @@ namespace Cmf.CLI.Objects
     /// <summary>
     /// Implementation for a service that return the current (running) CLI version
     /// </summary>
-    public class VersionService : IVersionService
+    public class VersionServices// : IVersionService
     {
-        public string PackageId => "@criticalmanufacturing/cli";
-        public string CurrentVersion => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "dev";
+        public string PackagseId => "@criticalmanufacturing/cli";
+        public string CurrentVsersion => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "dev";
     }
 }
