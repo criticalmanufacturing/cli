38237aa6926077b93ce251b59d0077191d6ce6b2
João Pereira <joaopereira@criticalmanufacturing.com>
2023-03-22 12:43:25 +0000

    feat(bump): support @angular/cli managed HTML packages

--- cmf-cli/Handlers/PackageType/HtmlNgCliPackageTypeHandler.cs
+++ cmf-cli/Handlers/PackageType/HtmlNgCliPackageTypeHandler.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions;
 using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text.RegularExpressions;
 using Cmf.CLI.Builders;
 using Cmf.CLI.Commands.restore;
+using Cmf.CLI.Core;
 using Cmf.CLI.Core.Enums;
 using Cmf.CLI.Core.Objects;
+using Cmf.CLI.Utilities;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Cmf.CLI.Handlers
@@ -15,11 +20,13 @@ namespace Cmf.CLI.Handlers
     /// <summary>
     /// Handler for packages managed with @angular/cli
     /// </summary>
-    /// <seealso cref="PresentationPackageTypeHandler" />
-    public class HtmlNgCliPackageTypeHandler : PresentationPackageTypeHandler
+    /// <seealso cref="PackageTypeHandler" />
+    public class HtmlNgCliPackageTypeHandler : PackageTypeHandler
     {
+        protected AngularWorkspace Workspace;
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="HtmlGulpPackageTypeHandler" /> class.
+        /// Initializes a new instance of the <see cref="HtmlNgCliPackageTypeHandler" /> class.
         /// </summary>
         /// <param name="cmfPackage"></param>
         public HtmlNgCliPackageTypeHandler(CmfPackage cmfPackage) : base(cmfPackage)
@@ -63,8 +70,8 @@ namespace Cmf.CLI.Handlers
             cmfPackage.DFPackageType = PackageType.Presentation;
 
             // Projects BuildSteps
-            var workspace = new AngularWorkspace(cmfPackage.GetFileInfo().Directory);
-            foreach (var package in workspace.PackagesToBuild)
+            Workspace = new AngularWorkspace(cmfPackage.GetFileInfo().Directory);
+            foreach (var package in Workspace.PackagesToBuild)
             {
                 BuildSteps = BuildSteps.Concat(new IBuildCommand[]
                 {
@@ -77,5 +84,54 @@ namespace Cmf.CLI.Handlers
                 }).ToArray();
             }
         }
+
+        /// <summary>
+        /// Bumps package.json and package-lock.json
+        /// of each project to the specified version.
+        /// </summary>
+        /// <param name="version">The version.</param>
+        /// <param name="buildNr">The version for build Nr.</param>
+        /// <param name="bumpInformation">The bump information.</param>
+        public override void Bump(string version, string buildNr, Dictionary<string, object> bumpInformation = null)
+        {
+            base.Bump(version, buildNr, bumpInformation);
+
+            foreach (var project in Workspace.Projects)
+            {
+                if (project.PackageJson.Content.version == null)
+                {
+                    throw new CliException(string.Format(CoreMessages.MissingMandatoryPropertyInFile, "version", project.PackageJson.File.FullName));
+                }
+                project.PackageJson.Content.version = GenericUtilities.RetrieveNewPresentationVersion(project.PackageJson.Content.version.ToString(), version, buildNr);
+
+                // write package.json
+                fileSystem.File.WriteAllText(project.PackageJson.File.FullName, JsonConvert.SerializeObject(project.PackageJson.Content, Formatting.Indented));
+
+                // In some cases we don't have the package-lock
+                if (project.PackageLock != null)
+                {
+                    if (project.PackageLock.Content.version == null)
+                    {
+                        throw new CliException(string.Format(CoreMessages.MissingMandatoryPropertyInFile, "version", project.PackageLock.File.FullName));
+                    }
+
+                    project.PackageLock.Content.version = project.PackageJson.Content.version;
+
+                    // if package-lock also refer the package in the packages list
+                    Dictionary<string, dynamic> packages = project.PackageLock.Content.packages.ToObject<Dictionary<string, dynamic>>();
+                    if(packages.ContainsKey("") && packages[""].name == project.Name)
+                    {
+                        packages[""].version = project.PackageJson.Content.version;
+                        project.PackageLock.Content.packages = JObject.FromObject(packages);
+                    }
+
+
+                    // write package-lock.json
+                    fileSystem.File.WriteAllText(project.PackageLock.File.FullName, JsonConvert.SerializeObject(project.PackageLock.Content, Formatting.Indented));
+                }
+
+            }
+        }
+
     }
 }
