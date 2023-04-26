9a3f690792d4f618ce7e3051dfff5429a9d375c5
Marco <marcosilva@criticalmanufacturing.com>
2023-04-26 20:24:11 +0100

    chore: replace values with tokens in index.html, ngsw.json and assets/config.json

--- cmf-cli/Handlers/PackageType/HtmlNgCliPackageTypeHandler.cs
+++ cmf-cli/Handlers/PackageType/HtmlNgCliPackageTypeHandler.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.IO.Abstractions;
 using System.Linq;
-using System.ServiceModel.Syndication;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Cmf.CLI.Builders;
 using Cmf.CLI.Commands.restore;
+using Cmf.CLI.Constants;
 using Cmf.CLI.Core;
 using Cmf.CLI.Core.Enums;
 using Cmf.CLI.Core.Objects;
 using Cmf.CLI.Utilities;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Cmf.CLI.Handlers
 {
@@ -42,13 +41,28 @@ namespace Cmf.CLI.Handlers
                     {
                         new Step(StepType.DeployFiles)
                         {
-                            ContentPath = "**"
+                            ContentPath = "**" // TODO: remove assets/config.json from the deployed files, should only apply transform
+                        },
+                        new Step(StepType.TaggedFile)
+                        {
+                            ContentPath = "index.html",
+                            TagFile = true
+                        },
+                        new Step(StepType.TaggedFile)
+                        {
+                            ContentPath = "ngsw.json",
+                            TagFile = true
+                        },
+                        new Step(StepType.TaggedFile)
+                        {
+                            ContentPath = "assets/config.json",
+                            TagFile = true
                         }
                     }
             );
 
             BuildSteps = new IBuildCommand[]
-{
+            {
                 new ExecuteCommand<RestoreCommand>()
                 {
                     Command = new RestoreCommand(),
@@ -65,12 +79,13 @@ namespace Cmf.CLI.Handlers
                     Args = new []{ "--force" },
                     WorkingDirectory = cmfPackage.GetFileInfo().Directory
                 }
-};
+            };
 
             cmfPackage.DFPackageType = PackageType.Presentation;
 
             // Projects BuildSteps
             Workspace = new AngularWorkspace(cmfPackage.GetFileInfo().Directory);
+            var apps = Workspace.Projects.Where(lib => lib.Type == "application").Select(p => p.Name).ToList();
             foreach (var package in Workspace.PackagesToBuild)
             {
                 BuildSteps = BuildSteps.Concat(new IBuildCommand[]
@@ -79,10 +94,13 @@ namespace Cmf.CLI.Handlers
                     {
                         DisplayName = $"ng build {package.Key}",
                         Command = "build",
+                        Args = apps.Contains(package.Key) ? new []{ "--base-href", "$(APPLICATION_BASE_HREF)" } : Array.Empty<string>(),
                         WorkingDirectory = package.Value
                     }
                 }).ToArray();
             }
+
+            BuildSteps = BuildSteps.Append(new NgBuildFileTokenReplacer(this.fileSystem, apps, cmfPackage)).ToArray();
         }
 
         /// <summary>
@@ -130,6 +148,168 @@ namespace Cmf.CLI.Handlers
                     fileSystem.File.WriteAllText(project.PackageLock.File.FullName, JsonConvert.SerializeObject(project.PackageLock.Content, Formatting.Indented));
                 }
 
+            }
+        }
+
+        // TODO: enable this when we can transform config.json
+        // public override void Pack(IDirectoryInfo packageOutputDir, IDirectoryInfo outputDir)
+        // {
+        //     var filesToPack = GetContentToPack(packageOutputDir);
+        //     if (CmfPackage.ContentToPack.HasAny() && !filesToPack.HasAny())
+        //     {
+        //         throw new CliException(string.Format(CoreMessages.ContentToPackNotFound, CmfPackage.PackageId, CmfPackage.Version));
+        //     }
+        //
+        //     if (filesToPack != null)
+        //     {
+        //         FilesToPack.AddRange(filesToPack);
+        //
+        //         FilesToPack.ForEach(fileToPack =>
+        //         {
+        //             Log.Debug($"Packing '{fileToPack.Source.FullName} to {fileToPack.Target.FullName} by contentToPack rule (Action: {fileToPack.ContentToPack.Action.ToString()}, Source: {fileToPack.ContentToPack.Source}, Target: {fileToPack.ContentToPack.Target})");
+        //             IDirectoryInfo _targetFolder = this.fileSystem.DirectoryInfo.New(fileToPack.Target.Directory.FullName);
+        //             if (!_targetFolder.Exists)
+        //             {
+        //                 _targetFolder.Create();
+        //             }
+        //         });
+        //     }
+        //
+        //     // TODO: replace this with an ignore entry in the ContentToPack of the template cmfpackage.json
+        //     FilesToPack = FilesToPack.Where(ftp => !ftp.Source.FullName.EndsWith($"assets{this.fileSystem.Path.DirectorySeparatorChar}config.json")).ToList();
+        //     GeneratePresentationConfigFile(packageOutputDir);
+        //
+        //     GenerateDeploymentFrameworkManifest(packageOutputDir);
+        //
+        //     FinalArchive(packageOutputDir, outputDir);
+        //
+        //     Log.Information($"{outputDir.FullName}/{CmfPackage.ZipPackageName} created");
+        //     
+        // }
+        
+        /// <summary>
+        /// Generates the presentation configuration file.
+        /// </summary>
+        /// <param name="packageOutputDir">The package output dir.</param>
+        private void GeneratePresentationConfigFile(IDirectoryInfo packageOutputDir)
+        {
+            Log.Debug("Generating Presentation config.json");
+            string path = $"{packageOutputDir.FullName}/assets/{CliConstants.CmfPackagePresentationConfig}";
+            
+            List<string> transformInjections = new();
+
+            IDirectoryInfo cmfPackageDirectory = CmfPackage.GetFileInfo().Directory;
+
+            foreach (ContentToPack contentToPack in CmfPackage.ContentToPack)
+            {
+                if (contentToPack.Action == PackAction.Transform)
+                {
+                    transformInjections.Add(contentToPack.Source);
+                }
+            }
+
+            // Get Template
+            string fileContent = ResourceUtilities.GetEmbeddedResourceContent($"{CliConstants.FolderTemplates}/{CmfPackage.PackageType}/config.ng.json");
+
+            fileContent = fileContent.Replace(CliConstants.TokenVersion, CmfPackage.Version);
+
+            string injection = string.Empty;
+            if (transformInjections.HasAny())
+            {
+                // we actually want a trailing comma here, because the inject token is in the middle of the document. If this changes we need to put more logic here.
+                var injections = transformInjections.Select(injection => this.fileSystem.File.ReadAllText($"{cmfPackageDirectory}/{injection}") + ",");
+                injection = string.Join(System.Environment.NewLine, injections);
+            }
+            fileContent = fileContent.Replace(CliConstants.TokenJDTInjection, injection);
+            fileContent = fileContent.Replace(CliConstants.CacheId, DateTime.Now.ToString("yyyyMMddHHmmss"));
+
+            this.fileSystem.File.WriteAllText(path, fileContent);
+        }
+
+        private class NgBuildFileTokenReplacer : IBuildCommand
+        {
+            private readonly IFileSystem fileSystem;
+            private readonly List<string> apps;
+            private readonly CmfPackage cmfPackage;
+
+            public NgBuildFileTokenReplacer(IFileSystem fileSystem, List<string> apps, CmfPackage cmfPackage)
+            {
+                this.fileSystem = fileSystem;
+                this.apps = apps;
+                this.cmfPackage = cmfPackage;
+            }
+            public string DisplayName
+            {
+                get { return "ng build file replacer"; }
+                set { throw new NotImplementedException();  }
+            }
+            public bool Test
+            {
+                get { return false; } 
+                set { } 
+            }
+            public Task Exec()
+            {
+                foreach (var app in apps)
+                {
+                    // place tokens in assets/config.json
+                    var configJsonPath = this.fileSystem.FileInfo.New($"dist/{app}/assets/config.json");
+                    if (configJsonPath.Exists)
+                    {
+                        Log.Debug($"Placing tokens in config.json at {configJsonPath.FullName}");
+                        var configJsonContent = configJsonPath.ReadToString();
+                        // TODO: replace app ref if we're packaging an app with the app name
+                        // configJsonContent = Regex.Replace(configJsonContent, @"""ref"":\s*""[^""]*""", "\"ref\": \"$(APPLICATION_REF)\"");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""name"":\s*""[^""]*""", "\"name\": \"$(TENANT_NAME)\"");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""enableSsl"":\s*[^,]*,", "\"enableSsl\": $(APPLICATION_PUBLIC_HTTP_TLS_ENABLED),");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""address"":\s*""[^""]*""", "\"address\": \"$(APPLICATION_PUBLIC_HTTP_ADDRESS)\"");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""port"":\s*[^,]*,", "\"port\": $(APPLICATION_PUBLIC_HTTP_PORT),");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""environmentName"":\s*""[^""]*""", "\"environmentName\": \"$(SYSTEM_NAME)\"");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""defaultDomain"":\s*""[^""]*""", "\"defaultDomain\": \"$(SECURITY_PORTAL_STRATEGY_LOCAL_AD_DEFAULT_DOMAIN)\"");
+                        configJsonContent = Regex.Replace(configJsonContent, @"""version"":\s*""[^""]*""", $"\"version\": \"{ExecutionContext.Instance.ProjectConfig.MESVersion}\",\n\"customizationVersion\": \"{cmfPackage.Version}\"");
+                        this.fileSystem.File.WriteAllText(configJsonPath.FullName, configJsonContent);
+                    }
+                    else
+                    {
+                        Log.Warning($"Couldn't find config.json at {configJsonPath.FullName}!");
+                    }
+                    
+                    
+                    if (ExecutionContext.Instance.ProjectConfig.RepositoryType == RepositoryType.App)
+                    {
+                        // place app name in index.html
+                        // TODO get app name from cmfapp.json
+                        var appName = ExecutionContext.Instance.ProjectConfig.ProjectName;
+                        var indexPath = this.fileSystem.FileInfo.New($"dist/{app}/index.html");
+                        if (indexPath.Exists)
+                        {
+                            Log.Debug($"Setting App Name '{appName}' as title in {indexPath.FullName}");
+                            var indexContent = indexPath.ReadToString();
+                            indexContent = Regex.Replace(indexContent, @"<title>.*</title>", $"<title>{appName}</title>");
+                            this.fileSystem.File.WriteAllText(indexPath.FullName, indexContent);
+                        }
+                        else
+                        {
+                            Log.Warning($"Couldn't find index.html at {indexPath.FullName}!");
+                        }
+                    }
+                    
+                    // fix trailing slash in ngsw.json
+                    var ngswPath = this.fileSystem.FileInfo.New($"dist/{app}/ngsw.json");
+                    if (ngswPath.Exists)
+                    {
+                        Log.Debug($"Placing tokens in ngsw.json at {ngswPath.FullName}");
+                        var ngswContent = ngswPath.ReadToString(); //  '\${APPLICATION_BASE_HREF}/', '${APPLICATION_BASE_HREF}';
+                        ngswContent = Regex.Replace(ngswContent, @"\$\(APPLICATION_BASE_HREF\)/", "$(APPLICATION_BASE_HREF)");
+                        this.fileSystem.File.WriteAllText(ngswPath.FullName, ngswContent);
+                    }
+                    else
+                    {
+                        Log.Warning($"Couldn't find ngsw.json at {ngswPath.FullName}!");
+                    }
+                }
+
+                return null;
             }
         }
 
