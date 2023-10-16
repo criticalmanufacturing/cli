d8c1c538aafdc1aa28fa249f2f53b760ae71ccf5
Roque <32566150+jrk94@users.noreply.github.com>
2023-10-16 17:55:50 +0100

    feat(new): add support for IoT Drivers

--- cmf-cli/Commands/new/IoTCommand.cs
+++ cmf-cli/Commands/new/IoTCommand.cs
@@ -5,6 +5,7 @@ using Cmf.CLI.Core.Attributes;
 using Cmf.CLI.Core.Enums;
 using Cmf.CLI.Core.Objects;
 using Cmf.CLI.Utilities;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
@@ -168,24 +169,37 @@ namespace Cmf.CLI.Commands.New
                 ForceColorOutput = false
             }.Exec();
 
-            // Install IoT Yeoman
-            Log.Debug($"Installing Yeoman");
+            // Add package.json extra bits
+            Log.Debug($"Overriding package.json");
 
-            new NPMCommand()
+            var rootPkgJsonPath = this.fileSystem.Path.Join(iotCustomPackageWorkDir.FullName, "package.json");
+            var packagejson = fileSystem.File.ReadAllText(rootPkgJsonPath);
+            dynamic rootPkgJson = JsonConvert.DeserializeObject(packagejson);
+
+            if (rootPkgJson == null)
             {
-                DisplayName = "npm yeoman",
-                Args = new string[] { "install", "yo", "--save-dev" },
-                WorkingDirectory = iotCustomPackageWorkDir
-            }.Exec();
+                throw new CliException("Could not load package.json");
+            }
 
-            Log.Debug($"Installing Yeoman IoT Generator");
+            rootPkgJson.scripts["build:libs"] = "npm run build -ws && gulp build";
+            rootPkgJson.scripts["postinstall"] = "npm run postinstall:graceful-fs && npm run postinstall:typescript";
+            rootPkgJson.scripts["postinstall:graceful-fs"] = "rimraf node_modules\\vinyl-fs\\node_modules\\graceful-fs";
+            rootPkgJson.scripts["postinstall:typescript"] = "rimraf node_modules\\@criticalmanufacturing\\dev-tasks\\node_modules\\typescript";
 
-            new NPMCommand()
-            {
-                DisplayName = "npm yeoman generator-iot",
-                Args = new string[] { "install", $"@criticalmanufacturing/generator-iot@{mesVersion.Major}{mesVersion.Minor}x", "--save-dev" },
-                WorkingDirectory = iotCustomPackageWorkDir
-            }.Exec();
+            rootPkgJson.devDependencies["@criticalmanufacturing/dev-tasks"] = "dev";
+            rootPkgJson.devDependencies["@criticalmanufacturing/generator-iot"] = $@"{mesVersion.Major}{mesVersion.Minor}x";
+
+            rootPkgJson.devDependencies["yo"] = "^4.3.1";
+            rootPkgJson.devDependencies["gulp"] = "3.9.1";
+            rootPkgJson.devDependencies["rimraf"] = "^4.1.2";
+            rootPkgJson.devDependencies["run-sequence"] = "2.2.1";
+            rootPkgJson.devDependencies["graceful-fs"] = "4.2.10";
+            rootPkgJson.devDependencies["concurrently"] = "^7.6.0";
+
+            packagejson = JsonConvert.SerializeObject(rootPkgJson, Formatting.Indented);
+            this.fileSystem.File.WriteAllText(rootPkgJsonPath, packagejson);
+
+            Log.Debug($"Overrided package.json");
 
             #region Link To HTML Package
 
---
+++ cmf-cli/resources/template_feed/iot/from1000/IoT.Package/%iotpackages%/gulpfile.js
@@ -0,0 +1,164 @@
+var gulp = require('gulp'),
+    rootUtils = require("@criticalmanufacturing/dev-tasks/root.main"),
+    pluginRunSequence = require('run-sequence'),
+    pluginYargs = require('yargs').argv,
+    _config = require('./.dev-tasks.json'),
+    _packagesPath = './projects/', //note the ending /
+    _dependenciesPath = './dependencies/',
+    _framework = _config.framework,
+    //NOTE: order matters in these arrays!
+    _dependencies = _config.dependencies,
+    _packages = _config.packages,
+    _apps = [`${_config.webAppPrefix}.web`],
+    tasks = null,
+    pluginUtil = require('gulp-util');
+applyOps = null;
+
+if (typeof _framework === "string" && _framework !== "") {
+    tasks = require("./projects/" + _framework + "/gulpfile.js")(gulp, _framework);
+}
+if (_dependencies != null && _dependencies.length > 0) {
+    _dependencies.forEach(function (dep) {
+        require(_dependenciesPath + dep + "/gulpfile.js")(gulp, dep);
+    });
+}
+if (_packages != null && _packages.length > 0) {
+    _packages.forEach(function (pkg) {
+        require(_packagesPath + pkg + "/gulpfile.js")(gulp, pkg);
+    });
+}
+
+applyOps = function (actions) {
+    if (!Array.isArray(actions)) { actions = [actions]; }
+    var operations = [];
+    var dependencyOperations = [];
+    var packageOperations = [];
+    if (_dependencies != null && _dependencies.length > 0) {
+        _dependencies.forEach(function (dep) {
+            actions.forEach(function (action) {
+                operations.push(dep + ">" + action);
+            });
+        });
+    }
+
+    actions.forEach(function (action) {
+        if (typeof _framework === "string" && _framework !== "") {
+            operations.push(_framework + '>' + action);
+        }
+    })
+
+    if (_packages != null && _packages.length > 0) {
+        _packages.forEach(function (mod) {
+            actions.forEach(function (action) {
+                operations.push(mod + ">" + action)
+            });
+        });
+    }
+
+    return operations;
+};
+
+/*
+ * Build all
+ */
+gulp.task('build', function (callback) {
+    if (_packages != null && _packages.length > 0) {
+        if (pluginYargs.server) {
+            var isWebAppCompilable = true;
+            rootUtils.runOperation(__dirname, _dependencies, _framework, _packages, _apps, "build", callback, typeof _framework === "string" && _framework !== "", isWebAppCompilable);
+        } else {
+            var ops = []
+            if (pluginYargs.parallelBuild !== false) {
+                pluginUtil.log(pluginUtil.colors.yellow(`building in parallel`));
+                if (Number.isInteger(pluginYargs.parallelBuild)) {
+                    pluginUtil.log(pluginUtil.colors.yellow(`Using ${pluginYargs.parallelBuild} tasks in parallel`));
+                    // split in tasks
+                    var opsToSliceArray = applyOps('build');
+                    for (i = 0; i < opsToSliceArray.length; i += pluginYargs.parallelBuild) {
+                        ops.push(opsToSliceArray.slice(i, i + pluginYargs.parallelBuild));
+                    }
+                } else {
+                    // all the tasks at the same time!
+                    ops = [applyOps('build')]
+                }
+            } else {
+                ops = applyOps('build');
+            }
+            // On customized projects we would only require to compile the web if the project defined a framework on their own
+            var isWebAppCompilable = _config.isWebAppCompilable;
+            if (isWebAppCompilable === true) {
+                ops = ops.concat([`${_config.webAppPrefix}.web>build`]);
+            }
+
+            ops = ops.concat(callback);
+            pluginRunSequence.apply(this, ops);
+        }
+    }
+});
+
+/*
+ * Install all
+ */
+gulp.task('install', function (callback) {
+    if (_packages != null && _packages.length > 0) {
+        if (pluginYargs.server) {
+            rootUtils.runOperation(__dirname, _dependencies, _framework, _packages, _apps, "install", callback, typeof _framework === "string" && _framework !== "", true);
+
+        } else {
+            var ops = applyOps(['install']);
+            // ops.unshift(`${_config.webAppPrefix}.web>install`);
+            ops = ops.concat(callback);
+            pluginRunSequence.apply(this, ops);
+        }
+    }
+});
+
+/*
+ * Install and build apps
+ */
+gulp.task('apps', function (callback) { pluginRunSequence.apply(this, ['build', `${_config.webAppPrefix}.web>build`, callback]); });
+
+/*
+ * start serving the web app
+ */
+gulp.task('start', function (callback) { pluginRunSequence(`${_config.webAppPrefix}.web>start`); });
+
+/*
+ * start running the tests
+ */
+gulp.task('test', function (callback) { pluginRunSequence.apply(this, applyOps('test').concat(callback)); });
+
+/*
+ * clean the workspace
+ */
+gulp.task('clean', function (callback) { pluginRunSequence.apply(this, applyOps('clean').concat(callback)); })
+
+gulp.task('clean-libs', function (callback) { pluginRunSequence.apply(this, applyOps('clean-libs').concat(callback)); })
+
+gulp.task('purge', function (callback) { pluginRunSequence.apply(this, applyOps('purge').concat(callback)); })
+
+gulp.task('watch', function (callback) { pluginRunSequence.apply(this, applyOps('watch').concat(callback)); });
+
+/**
+ * i18n generators
+ */
+gulp.task('create-missing-i18n', function (callback) { pluginRunSequence.apply(this, applyOps('create-missing-i18n').concat(callback)); });
+gulp.task('i18n-ts2po', function (callback) { pluginRunSequence.apply(this, applyOps('i18n-ts2po').concat(callback)); });
+gulp.task('i18n-po2ts', function (callback) { pluginRunSequence.apply(this, applyOps('i18n-po2ts').concat(callback)); });
+
+/**
+ * CI
+ */
+gulp.task('publish', function (callback) { pluginRunSequence.apply(this, applyOps('publish').concat(callback)); });
+
+gulp.task('check-version', function (callback) { pluginRunSequence.apply(this, applyOps('check-version').concat(callback)); });
+gulp.task('set-version', function (callback) { pluginRunSequence.apply(this, applyOps('set-version').concat(callback)); });
+gulp.task('npm-dist-tag-add', function (callback) { pluginRunSequence.apply(this, applyOps('npm-dist-tag-add').concat(callback)); });
+gulp.task('npm-dist-tag-rm', function (callback) { pluginRunSequence.apply(this, applyOps('npm-dist-tag-rm').concat(callback)); });
+gulp.task('npm-dist-tag-ls', function (callback) { pluginRunSequence.apply(this, applyOps('npm-dist-tag-ls').concat(callback)); });
+gulp.task('npm-dist-tag-copy-version', function (callback) { pluginRunSequence.apply(this, applyOps('npm-dist-tag-copy-version').concat(callback)); });
+
+gulp.task('bump-version', function (callback) { pluginRunSequence.apply(this, applyOps('bump-version').concat(callback)); });
+gulp.task('generate-package-lock', function (callback) { pluginRunSequence.apply(this, applyOps('generate-package-lock').concat(callback)); });
+
+gulp.task('check-circular-imports', function (callback) { pluginRunSequence.apply(this, applyOps('check-circular-imports').concat(callback)); });
