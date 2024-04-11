0a0d9ed8398aa5035ef703183ddf2f70a01b21b0
Jonas Maia <JonasMaia@criticalmanufacturing.com>
2024-04-11 14:29:45 +0100

    feat(init): support cmfapp.json generation

---
+++ core/Utilities/AppIconUtilities.cs
@@ -0,0 +1,50 @@
+﻿using Cmf.CLI.Utilities;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Cmf.CLI.Core.Utilities
+{
+    /// <summary>
+    /// Icon utilities for cmf apps
+    /// </summary>
+    public static class AppIconUtilities
+    {
+        /// <summary>
+        /// Checks if the specified icon image is of the correct type (PNG) and shape (square).
+        /// </summary>
+        /// <param name="path">The file path of the icon image to validate.</param>
+        /// <returns>True if the icon is valid; otherwise, false.</returns>
+        /// <exception cref="FileNotFoundException">Thrown when the specified file path does not exist.</exception>
+        /// <exception cref="CliException">
+        /// Thrown when the icon is not in PNG format or when it is not square shaped.
+        /// </exception>
+        public static bool IsIconValid(string path)
+        {
+            const string PNG_EXTENSION = "png";
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("File not found.", path);
+            }
+
+            using SixLabors.ImageSharp.Image image = SixLabors.ImageSharp.Image.Load(path);
+
+            bool isPNG = image.Metadata.DecodedImageFormat.FileExtensions.Contains(PNG_EXTENSION);
+
+            if (!isPNG)
+            {
+                throw new CliException("The icon provided is not PNG");
+            }
+
+            bool isSquareShaped = image.Height == image.Width;
+
+            if (!isSquareShaped)
+            {
+                throw new CliException("The icon provided is not square shaped");
+            }
+
+            return true;
+        }
+    }
+}
