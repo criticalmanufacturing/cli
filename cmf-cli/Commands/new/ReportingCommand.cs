ac3c9f5be71d3737bfa08f00751e21f00f22d9e2
João Pereira <joaopereira@criticalmanufacturing.com>
2024-05-16 17:02:07 +0100

    feat(scaffolding): move reporting package to root

---
+++ cmf-cli/Commands/new/ReportingCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Cmf.CLI.Core.Attributes;
+using Cmf.CLI.Core.Enums;
+
+namespace Cmf.CLI.Commands.New
+{
+    /// <summary>
+    /// Generates Reporting package structure
+    /// </summary>
+    [CmfCommand("reporting", ParentId = "new")]
+    public class ReportingCommand : LayerTemplateCommand
+    {
+        /// <inheritdoc />
+        public ReportingCommand() : base("reporting", PackageType.Reporting)
+        {
+            this.registerInParent = true;
+        }
+
+        /// <inheritdoc />
+        public ReportingCommand(IFileSystem fileSystem) : base("reporting", PackageType.Reporting, fileSystem)
+        {
+            this.registerInParent = true;
+        }
+
+        /// <inheritdoc />
+        protected override List<string> GenerateArgs(IDirectoryInfo projectRoot, IDirectoryInfo workingDir, List<string> args)
+        {
+            var relativePathToRoot =
+                this.fileSystem.Path.Join("..", "..", //always two levels deeper
+                    this.fileSystem.Path.GetRelativePath(
+                        workingDir.FullName,
+                        projectRoot.FullName)
+                ).Replace("\\", "/");
+
+            args.AddRange(new[]
+            {
+                "--rootRelativePath", relativePathToRoot
+            });
+
+            return args;
+        }
+    }
+}
