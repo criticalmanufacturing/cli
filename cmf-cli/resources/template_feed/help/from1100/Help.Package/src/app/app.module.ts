f922f44b4c4b6158c4ae262d71c90411b02f4bfe
Xavier Coelho <XavierCoelho@criticalmanufacturing.com>
2024-09-04 18:35:46 +0100

    fix(new help): add v11 help template

---
+++ cmf-cli/resources/template_feed/help/from1100/Help.Package/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MetadataRoutingModule } from 'cmf-core';
+import { CoreControlsMetadataModule } from 'cmf-core-controls/metadata';
+
+import { DocsControlsMetadataModule } from 'cmf-docs-controls/metadata';
+
+import { DocsShellMetadataModule } from 'cmf-docs-shell/metadata';
+import { DocsAreaDesignsystemMetadataModule } from 'cmf-docs-area-designsystem/metadata';
+
+// start: add your custom doc package imports here
+import { DocsArea<%= $CLI_PARAM_DFPackageNamePascalCase %>MetadataModule } from '<%= $CLI_PARAM_CustomAssetsPackageName %>/metadata';
+// end: add your custom doc package imports here
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        CoreControlsMetadataModule,
+        DocsShellMetadataModule,
+        DocsControlsMetadataModule,
+        DocsAreaDesignsystemMetadataModule,
+        // start: add your custom doc package modules here
+        DocsArea<%= $CLI_PARAM_DFPackageNamePascalCase %>MetadataModule,
+        // end: add your custom doc package modules here
+        MetadataRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
