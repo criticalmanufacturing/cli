e883568085ffaabbb2c05567f466f4e966b09064
João Octávio Pereira <joaopereira@criticalmanufacturing.com>
2024-11-29 10:42:54 +0000

    feat(restore): add cifs client to unc paths usage to work on any OS

---
+++ core/Interfaces/ICIFSClient.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Core.Objects;
+
+namespace Cmf.CLI.Core.Interfaces
+{
+    public interface ICIFSClient
+    {
+        public string Server { get; }
+        public List<SharedFolder> SharedFolders { get;}
+        public bool IsConnected { get; }
+        public void Connect();
+        public void Disconnect();
+    }
+}
