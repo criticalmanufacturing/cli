67f35d247c5c4eda8a963582a5489a8bcc9512b8
Pedro Silva <PedroSilva@criticalmanufacturing.com>
2025-05-08 10:04:07 +0100

    fix: avoid npx interactive confirmation prompt when installing packages

--- cmf-cli/Builders/NPXCommand.cs
+++ cmf-cli/Builders/NPXCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -34,8 +35,30 @@ namespace Cmf.CLI.Builders
         /// <inheritdoc />
         public override ProcessBuildStep[] GetSteps()
         {
-            var args = this.Args?.ToList() ?? new List<string>();
-            args.Insert(0, this.Command);
+            var args = new List<string>
+            {
+                // Bypass any interactive confirmation prompts that might appear
+                "-y"
+            };
+
+            // The initial versions of NPX (<= 6) that are used by MES v8 and v9 must be called like this:
+            //      npx -y true
+            // Later versions of NPX (>= 7) that are used by MES v10 and greater, must be called only like this:
+            //      npx -y
+            // So we get the MES version of the project, and we assume that the user must be running the supported
+            // node version for that MES project
+            var mesVersion = ExecutionContext.Instance?.ProjectConfig?.MESVersion;
+            if (mesVersion != null && mesVersion < new Version(10, 0)) 
+            {
+                args.Add("true");
+            }
+ 
+            args.Add(this.Command);
+            if (this.Args != null) 
+            {
+                args.AddRange(this.Args);
+            }
+
             if (this.ForceColorOutput)
             {
                 args.AddRange(new[] { "--color", "always" });
