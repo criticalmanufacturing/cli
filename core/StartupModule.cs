6290761e61e70d8f9ada00bb804a89e7e8dbcd8a
Manuel Martins <ManuelMartins@criticalmanufacturing.com>
2026-02-05 10:00:24 +0000

    chore(core): bump core System.CommandLine to 2.0.2

--- core/StartupModule.cs
+++ core/StartupModule.cs
@@ -23,7 +23,7 @@ namespace Cmf.CLI.Core
         /// <param name="args">ar</param>
         /// <param name="npmClient">The NPM client. if is not set, we assume NPMClient implementation by default</param>
         /// <param name="registerExtraServices">function to add extra services to the ServiceProvider</param>
-        public static async Task<RootCommand> Configure(string packageId, string envVarPrefix, string description, string[] args, INPMClient? npmClient = null, Action<IServiceCollection>? registerExtraServices = null)
+        public static async Task<RootCommand> Configure(string packageId, string envVarPrefix, string description, string[] args, INPMClient npmClient = null, Action<IServiceCollection> registerExtraServices = null)
         {
             // in a scenario that cli is not running on a terminal,
             // the AnsiConsole.Profile.Width defaults to 80,which is a low value and causes unexpected break lines.
