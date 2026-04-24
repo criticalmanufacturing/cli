69fb5c5c00278c2cfe0d1503db00d45fba99999c
Pedro Pinho <pedro1998pinho@gmail.com>
2026-04-24 14:23:45 +0000

    feat: update PublishCommand to handle multiple package files and also directories

--- cmf-cli/Commands/publish/PublishCommand.cs
+++ cmf-cli/Commands/publish/PublishCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.CommandLine;
 using System.IO.Abstractions;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Cmf.CLI.Core;
@@ -34,12 +35,12 @@ public class PublishCommand : BaseCommand
 
     public override void Configure(Command cmd)
     {
-        var fileArgument = new Argument<IFileInfo>("file")
+        var packagesArgument = new Argument<string[]>("packagePaths")
         {
-            Description = "Package file",
-            CustomParser = argResult => Parse<IFileInfo>(argResult)
+            Description = "Package file(s) (.zip or .tgz) or folder(s) containing package files",
+            Arity = ArgumentArity.OneOrMore
         };
-        cmd.Add(fileArgument);
+        cmd.Add(packagesArgument);
 
         var ciOption = new Option<bool>("--ci")
         {
@@ -52,6 +53,12 @@ public class PublishCommand : BaseCommand
             Description = "Use the first non-CI repository URL from the repositories file"
         };
         cmd.Add(releaseOption);
+
+        var dryRunOption = new Option<bool>("--dry-run")
+        {
+            Description = "Resolve packages and print what would be published without uploading anything"
+        };
+        cmd.Add(dryRunOption);
 
         var repositoryOption = new Option<Uri>("--repository")
         {
@@ -66,36 +73,35 @@ public class PublishCommand : BaseCommand
         // Add the handler
         cmd.SetAction((parseResult, cancellationToken) =>
         {
-            var file = parseResult.GetValue(fileArgument);
+            var packages = parseResult.GetValue(packagesArgument);
             var repository = parseResult.GetValue(repositoryOption);
             var ci = parseResult.GetValue(ciOption);
             var release = parseResult.GetValue(releaseOption);
+            var dryRun = parseResult.GetValue(dryRunOption);
 
-            Execute(file, repository, ci, release);
+            Execute(packages, repository, ci, release, dryRun);
             return Task.FromResult(0);
         });
     }
 
-    public void Execute(IFileInfo file, Uri repository, bool ci, bool release)
+    public void Execute(string packagePath, Uri repository, bool ci, bool release)
+    {
+        Execute([packagePath], repository, ci, release, false);
+    }
+
+    public void Execute(string[] packagePaths, Uri repository, bool ci, bool release)
+    {
+        Execute(packagePaths, repository, ci, release, false);
+    }
+
+    public void Execute(string[] packagePaths, Uri repository, bool ci, bool release, bool dryRun)
     {
         using var activity = ExecutionContext.ServiceProvider?.GetService<ITelemetryService>()?.StartExtendedActivity(this.GetType().Name);
-
-        if (!file.Exists || file.Directory == null)
-        {
-            throw new CliException(
-                $"Could not find package file {file.FullName}, make sure the file exists and is valid");
-        }
-
-        if (file.Extension != ".zip" && file.Extension != ".tgz")
-        {
-            throw new CliException(
-                $"The package needs to be in a zip or gzipped tar file (with .tgz extension). Use the `pack` command to get a valid file to publish.");
-        }
 
         if (ci && release)
         {
             throw new CliException(
-                $"Cannot use both flags `--ci` and `--release` at the same time.");
+                "Cannot use both flags `--ci` and `--release` at the same time.");
         }
 
         if ((ci || release) && repository != null)
@@ -112,7 +118,7 @@ public class PublishCommand : BaseCommand
             if (repository == null)
             {
                 throw new CliException(
-                    $"No CIRepository was defined on the repositories configuration file, cannot use the `--ci` flag.");
+                    "No CIRepository was defined on the repositories configuration file, cannot use the `--ci` flag.");
             }
         }
         else if (release)
@@ -122,34 +128,95 @@ public class PublishCommand : BaseCommand
             if (repository == null)
             {
                 throw new CliException(
-                    $"No Repositories were defined on the repositories configuration file, cannot use the `--release` flag.");
+                    "No Repositories were defined on the repositories configuration file, cannot use the `--release` flag.");
             }
         }
 
         if (repository == null)
         {
             throw new CliException(
-                $"No repository URL to publish to was passed. Try using one of the following options: `--ci`, `--release` or `--repository`.");
+                "No repository URL to publish to was passed. Try using one of the following options: `--ci`, `--release` or `--repository`.");
         }
 
-        // If it passes the above checks the only possible client for the requested file 
-        // is a ArchiveRepositoryClient with only a single Package
-        var client = ExecutionContext.ServiceProvider?.GetService<IRepositoryLocator>()
-            .GetRepositoryClient(new Uri(file.FullName), file.FileSystem) as ArchiveRepositoryClient;
-        if (client == null)
+        var repositoryLocator = ExecutionContext.ServiceProvider?.GetService<IRepositoryLocator>();
+        var packagesToPublish = new List<CmfPackageV1>();
+
+        foreach (var packagePath in packagePaths)
         {
-            throw new CliException($"Could not determine repository type for {file.FullName}!");
+            var directory = fileSystem.DirectoryInfo.New(packagePath);
+            var fileInfo = fileSystem.FileInfo.New(packagePath);
+
+            if (fileInfo.Exists)
+            {
+                if (fileInfo.Extension != ".zip" && fileInfo.Extension != ".tgz")
+                {
+                    throw new CliException(
+                        "The package needs to be in a zip or gzipped tar file (with .tgz extension). Use the `pack` command to get a valid file to publish.");
+                }
+
+                var fileClient = repositoryLocator?.GetRepositoryClient(new Uri(fileInfo.FullName), fileInfo.FileSystem) as ArchiveRepositoryClient;
+                if (fileClient == null)
+                {
+                    throw new CliException($"Could not determine repository type for {fileInfo.FullName}!");
+                }
+
+                Log.Debug($"Got client {fileClient.GetType().Name} for package file {fileInfo.FullName}");
+                packagesToPublish.Add(fileClient.List().GetAwaiter().GetResult().Single());
+                continue;
+            }
+
+            if (!directory.Exists)
+            {
+                throw new CliException(
+                    $"Could not find package file or directory at {packagePath}, make sure the path exists and is valid");
+            }
+
+            var directoryClient = new ArchiveRepositoryClient(directory.FullName, directory.FileSystem);
+            Log.Debug($"Got client {directoryClient.GetType().Name} for package directory {directory.FullName}");
+            var folderPackages = directoryClient.List().GetAwaiter().GetResult();
+
+            if (!folderPackages.Any())
+            {
+                Log.Information($"No packages found in folder {directory.FullName}. Skipping...");
+                continue;
+            }
+
+            Log.Information($"Found {folderPackages.Count} package(s) in folder {directory.FullName}.");
+            packagesToPublish.AddRange(folderPackages);
         }
-        Log.Debug($"Got client {client.GetType().Name} for package file {file.FullName}");
-        var repoClient = ExecutionContext.ServiceProvider?.GetService<IRepositoryLocator>()
-            .GetRepositoryClient(repository, file.FileSystem);
-        if (repoClient == null)
+        
+        if (!packagesToPublish.Any())
         {
-            throw new CliException($"Could not determine repository type for {repository.AbsoluteUri}!");
+            Log.Information($"No packages found to publish.");
         }
-        Log.Debug($"Got client {repoClient.GetType().Name} for repository URL {repository.AbsoluteUri}");
-        Log.Debug($"Publishing package with target repository client...");
-        repoClient.Put(client.List().Result.Single()).GetAwaiter().GetResult();
-        Log.Debug("Publish completed!");
+        else
+        {
+            var repoClient = repositoryLocator?.GetRepositoryClient(repository, fileSystem);
+            if (repoClient == null)
+            {
+                throw new CliException($"Could not determine repository type for {repository.AbsoluteUri}!");
+            }
+            Log.Debug($"Got client {repoClient.GetType().Name} for repository URL {repository.AbsoluteUri}");
+
+            foreach (var package in packagesToPublish)
+            {
+                var action = dryRun ? "Would publish" : "Publishing";
+                Log.Information($"{action} {package.PackageDotRef}...");
+
+                if (!dryRun)
+                {
+                    repoClient.Put(package).GetAwaiter().GetResult();
+                }
+            }
+
+            if (dryRun)
+            {
+                Log.Information($"Dry run completed. {packagesToPublish.Count} package(s) would be published.");
+            }
+            else
+            {
+                Log.Information($"Completed publishing {packagesToPublish.Count} package(s)!");
+            }
+        }
     }
 }
--- tests/Specs/Publish.cs
+++ tests/Specs/Publish.cs
@@ -5,6 +5,8 @@ using System.IO.Abstractions;
 using Moq;
 using Cmf.CLI.Commands;
 using System.Threading.Tasks;
+using Cmf.CLI.Core;
+using Cmf.CLI.Utilities;
 using System.Collections.Generic;
 using System.Formats.Tar;
 using System.IO.Abstractions.TestingHelpers;
@@ -17,6 +19,7 @@ using Cmf.CLI.Core.Interfaces;
 using Cmf.CLI.Core.Repository;
 using Cmf.CLI.Core.Services;
 using Cmf.CLI.Core.Repository.Credentials;
+using Spectre.Console;
 using FluentAssertions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -34,31 +37,32 @@ public class Publish
     public void Repository_Arg_ParsedCorrectly(string expectedHost, string inputRepository)
     {
         string inputFile = "/test/testPackage.zip";
+        string inputFolder = "/test/packages";
 
         var publishCommand = new PublishCommand();
         var cmd = new Command("publish");
         publishCommand.Configure(cmd);
 
-        var fileArg = cmd.Arguments.FirstOrDefault(a => a.Name == "file") as Argument<IFileInfo>;
+        var packagesArg = cmd.Arguments.FirstOrDefault(a => a.Name == "packagePaths") as Argument<string[]>;
         var repositoryOpt = cmd.Options.OfType<Option<Uri>>().FirstOrDefault();
 
-        IFileInfo _file = null;
+        string[] _packages = null;
         Uri _repository = null;
 
         cmd.SetAction((parseResult, cancellationToken) =>
         {
-            _file = parseResult.GetValue(fileArg);
+            _packages = parseResult.GetValue(packagesArg);
             _repository = parseResult.GetValue(repositoryOpt);
             return Task.FromResult(0);
         });
 
         var console = new TestConsole();
         var parseResult = cmd.Parse(new[] {
-            inputFile, "--repository", inputRepository
+            inputFile, inputFolder, "--repository", inputRepository
         });
         parseResult.Invoke(console);
 
-        Assert.Equal(inputFile, _file.FullName);
+        _packages.Should().Equal(inputFile, inputFolder);
         Assert.Equal(inputRepository, _repository.OriginalString);
         Assert.Equal(expectedHost, _repository.Host);
     }
@@ -120,7 +124,7 @@ public class Publish
 
         // Act
         var publishCommand = new PublishCommand(fileSystem);
-        publishCommand.Execute(fileSystem.FileInfo.New(archivePath), repositoryUrl, false, false);
+        publishCommand.Execute(archivePath, repositoryUrl, false, false);
 
         // Assert
         npmClient.Verify(x => x.PublishPackage(It.IsAny<IFileInfo>()), Times.Once);
@@ -218,7 +222,7 @@ public class Publish
 
         // Act
         var publishCommand = new PublishCommand(fileSystem);
-        publishCommand.Execute(fileSystem.FileInfo.New(archivePath), repository: null, ci: true, release: false);
+        publishCommand.Execute(archivePath, repository: null, ci: true, release: false);
 
         // Assert
         repositoryLocator.Verify(x => x.GetRepositoryClient(repositoryUrl, It.IsAny<IFileSystem>()), Times.Once);
@@ -280,9 +284,290 @@ public class Publish
 
         // Act
         var publishCommand = new PublishCommand(fileSystem);
-        var exception = publishCommand.Invoking(x => x.Execute(fileSystem.FileInfo.New(archivePath), repositoryUrl, false, false));
+        var exception = publishCommand.Invoking(x => x.Execute(archivePath, repositoryUrl, false, false));
 
         // Assert
         exception.Should().Throw<Exception>().WithMessage("*Invalid manifest file: one of the following keywords must be present*");
+    }
+
+    [Fact]
+    public void DryRunArgument_DoesNotPublishPackages()
+    {
+        // Arrange
+        var repositoryUrl = new Uri("https://fake.criticalmanufacturing.io");
+        var archivePath = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Packages\Cmf.Custom.Tests.1.1.0.zip");
+        var archiveData = CreatePackageArchiveData("1.1.0");
+
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            { archivePath, new MockFileData(archiveData) },
+        }, MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test"));
+
+        var remoteClientMock = new Mock<IRepositoryClient>();
+
+        var repositoryLocator = new Mock<IRepositoryLocator>();
+        repositoryLocator
+            .SetupSequence(m => m.GetRepositoryClient(It.IsAny<Uri>(), It.IsAny<IFileSystem>()))
+            .Returns(new ArchiveRepositoryClient(archivePath, fileSystem))
+            .Returns(remoteClientMock.Object);
+
+        ExecutionContext.ServiceProvider = (new ServiceCollection())
+            .AddSingleton<IFileSystem>(fileSystem)
+            .AddSingleton<IVersionService, MockVersionService>()
+            .AddSingleton<IRepositoryAuthStore>(RepositoryAuthStore.FromEnvironmentConfig(fileSystem))
+            .AddSingleton<IRepositoryLocator>(repositoryLocator.Object)
+            .AddSingleton<IRepositoryCredentials, NPMRepositoryCredentials>()
+            .BuildServiceProvider();
+        ExecutionContext.Initialize(fileSystem);
+
+        var publishCommand = new PublishCommand(fileSystem);
+        var cmd = new Command("publish");
+        publishCommand.Configure(cmd);
+
+        var console = new TestConsole();
+
+        // Act
+        var result = cmd.Parse(new[]
+        {
+            archivePath,
+            "--repository", repositoryUrl.AbsoluteUri,
+            "--dry-run"
+        }).Invoke(console);
+
+        // Assert
+        result.Should().Be(0);
+        remoteClientMock.Verify(x => x.Put(It.IsAny<CmfPackageV1>()), Times.Never);
+        repositoryLocator.Verify(x => x.GetRepositoryClient(repositoryUrl, It.IsAny<IFileSystem>()), Times.Once);
+    }
+
+    [Fact]
+    public void PublishFromDirectory()
+    {
+        // Arrange
+        var repositoryUrl = new Uri("https://fake.criticalmanufacturing.io");
+        var packageDir = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Package");
+        var topLevelArchive1 = MockUnixSupport.Path(packageDir + @"\Cmf.Custom.Tests.1.1.0.zip");
+        var topLevelArchive2 = MockUnixSupport.Path(packageDir + @"\Cmf.Custom.Tests.2.0.0.zip");
+        var nestedArchive = MockUnixSupport.Path(packageDir + @"\subfolder\Cmf.Custom.Tests.3.0.0.zip");
+        var archiveData1 = CreatePackageArchiveData("1.1.0");
+        var archiveData2 = CreatePackageArchiveData("2.0.0");
+        var archiveData3 = CreatePackageArchiveData("3.0.0");
+
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            { topLevelArchive1, new MockFileData(archiveData1) },
+            { topLevelArchive2, new MockFileData(archiveData2) },
+            { nestedArchive, new MockFileData(archiveData3) },
+        }, packageDir);
+
+        var publishedFilesCount = 0;
+        
+        var npmClient = new Mock<INPMClientEx>();
+        npmClient.Setup(x => x.PublishPackage(It.IsAny<IFileInfo>()))
+            .Callback(() => publishedFilesCount++);
+        
+        var repositoryLocator = new Mock<IRepositoryLocator>();
+        repositoryLocator
+            .Setup(m => m.GetRepositoryClient(It.IsAny<Uri>(), It.IsAny<IFileSystem>()))
+            .Returns((Uri uri, IFileSystem fs) =>
+            {
+                if (uri.IsFile)
+                {
+                    return new ArchiveRepositoryClient(uri.LocalPath, fs);
+                }
+                return new NPMRepositoryClient(repositoryUrl.AbsoluteUri, fs, npmClient.Object);
+            });
+        
+        ExecutionContext.ServiceProvider = (new ServiceCollection())
+            .AddSingleton<IFileSystem>(fileSystem)
+            .AddSingleton<IVersionService, MockVersionService>()
+            .AddSingleton<IRepositoryAuthStore>(RepositoryAuthStore.FromEnvironmentConfig(fileSystem))
+            .AddSingleton<IRepositoryLocator>(repositoryLocator.Object)
+            .AddSingleton<IRepositoryCredentials, NPMRepositoryCredentials>()
+            .BuildServiceProvider();
+        ExecutionContext.Initialize(fileSystem);
+
+        // Act
+        var publishCommand = new PublishCommand(fileSystem);
+        publishCommand.Execute(packageDir, repositoryUrl, false, false);
+
+        // Assert
+        npmClient.Verify(x => x.PublishPackage(It.IsAny<IFileInfo>()), Times.Exactly(2));
+        publishedFilesCount.Should().Be(2);
+    }
+
+    [Fact]
+    public void PublishMultipleFilesAndDirectories()
+    {
+        // Arrange
+        var repositoryUrl = new Uri("https://fake.criticalmanufacturing.io");
+        var packageDir = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Package");
+        var directArchive = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Packages\Cmf.Custom.Tests.0.9.0.zip");
+        var folderArchive = MockUnixSupport.Path(packageDir + @"\Cmf.Custom.Tests.1.1.0.zip");
+        var directArchiveData = CreatePackageArchiveData("0.9.0");
+        var folderArchiveData = CreatePackageArchiveData("1.1.0");
+
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            { directArchive, new MockFileData(directArchiveData) },
+            { folderArchive, new MockFileData(folderArchiveData) },
+        }, MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test"));
+
+        var publishedFilesCount = 0;
+
+        var npmClient = new Mock<INPMClientEx>();
+        npmClient.Setup(x => x.PublishPackage(It.IsAny<IFileInfo>()))
+            .Callback(() => publishedFilesCount++);
+
+        var repositoryLocator = new Mock<IRepositoryLocator>();
+        repositoryLocator
+            .Setup(m => m.GetRepositoryClient(It.IsAny<Uri>(), It.IsAny<IFileSystem>()))
+            .Returns((Uri uri, IFileSystem fs) =>
+            {
+                if (uri.IsFile)
+                {
+                    return new ArchiveRepositoryClient(uri.LocalPath, fs);
+                }
+                return new NPMRepositoryClient(repositoryUrl.AbsoluteUri, fs, npmClient.Object);
+            });
+
+        ExecutionContext.ServiceProvider = (new ServiceCollection())
+            .AddSingleton<IFileSystem>(fileSystem)
+            .AddSingleton<IVersionService, MockVersionService>()
+            .AddSingleton<IRepositoryAuthStore>(RepositoryAuthStore.FromEnvironmentConfig(fileSystem))
+            .AddSingleton<IRepositoryLocator>(repositoryLocator.Object)
+            .AddSingleton<IRepositoryCredentials, NPMRepositoryCredentials>()
+            .BuildServiceProvider();
+        ExecutionContext.Initialize(fileSystem);
+
+        // Act
+        var publishCommand = new PublishCommand(fileSystem);
+        publishCommand.Execute([directArchive, packageDir], repositoryUrl, false, false);
+
+        // Assert
+        npmClient.Verify(x => x.PublishPackage(It.IsAny<IFileInfo>()), Times.Exactly(2));
+        publishedFilesCount.Should().Be(2);
+    }
+
+    [Fact]
+    public void PublishFailsWithoutPublishingAnythingWhenAnyInputPathDoesNotExist()
+    {
+        // Arrange
+        var repositoryUrl = new Uri("https://fake.criticalmanufacturing.io");
+        var existingArchive = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Packages\Cmf.Custom.Tests.1.1.0.zip");
+        var missingArchive = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Packages\Missing.zip");
+        var archiveData = CreatePackageArchiveData("1.1.0");
+
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        {
+            { existingArchive, new MockFileData(archiveData) },
+        }, MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test"));
+
+        var npmClient = new Mock<INPMClientEx>();
+        var repositoryLocator = new Mock<IRepositoryLocator>();
+        repositoryLocator
+            .Setup(m => m.GetRepositoryClient(It.IsAny<Uri>(), It.IsAny<IFileSystem>()))
+            .Returns((Uri uri, IFileSystem fs) =>
+            {
+                if (uri.IsFile)
+                {
+                    return new ArchiveRepositoryClient(uri.LocalPath, fs);
+                }
+
+                return new NPMRepositoryClient(repositoryUrl.AbsoluteUri, fs, npmClient.Object);
+            });
+
+        ExecutionContext.ServiceProvider = (new ServiceCollection())
+            .AddSingleton<IFileSystem>(fileSystem)
+            .AddSingleton<IVersionService, MockVersionService>()
+            .AddSingleton<IRepositoryAuthStore>(RepositoryAuthStore.FromEnvironmentConfig(fileSystem))
+            .AddSingleton<IRepositoryLocator>(repositoryLocator.Object)
+            .AddSingleton<IRepositoryCredentials, NPMRepositoryCredentials>()
+            .BuildServiceProvider();
+        ExecutionContext.Initialize(fileSystem);
+
+        var publishCommand = new PublishCommand(fileSystem);
+
+        // Act
+        var exception = publishCommand.Invoking(x =>
+            x.Execute([existingArchive, missingArchive], repositoryUrl, false, false));
+
+        // Assert
+        exception.Should().Throw<CliException>()
+            .WithMessage($"*Could not find package file or directory at {missingArchive}*");
+        npmClient.Verify(x => x.PublishPackage(It.IsAny<IFileInfo>()), Times.Never);
+    }
+
+    [Fact]
+    public void PublishSkipsEmptyFolderAndLogsMessage()
+    {
+        // Arrange
+        var repositoryUrl = new Uri("https://fake.criticalmanufacturing.io");
+        var emptyFolder = MockUnixSupport.Path(@"C:\repo\Cmf.Custom.Test\Empty");
+        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>(), emptyFolder);
+        var npmClient = new Mock<INPMClientEx>();
+        var repositoryLocator = new Mock<IRepositoryLocator>();
+        repositoryLocator
+            .Setup(m => m.GetRepositoryClient(It.IsAny<Uri>(), It.IsAny<IFileSystem>()))
+            .Returns((Uri uri, IFileSystem fs) =>
+            {
+                if (uri.IsFile)
+                {
+                    return new ArchiveRepositoryClient(uri.LocalPath, fs);
+                }
+
+                return new NPMRepositoryClient(repositoryUrl.AbsoluteUri, fs, npmClient.Object);
+            });
+
+        var writer = new StringWriter();
+        Log.AnsiConsole = AnsiConsole.Create(new AnsiConsoleSettings
+        {
+            Ansi = AnsiSupport.Yes,
+            ColorSystem = (ColorSystemSupport)ColorSystem.NoColors,
+            Out = new AnsiConsoleOutput(writer),
+            Interactive = InteractionSupport.No,
+            Enrichment = new ProfileEnrichment
+            {
+                UseDefaultEnrichers = false,
+            },
+        });
+
+        ExecutionContext.ServiceProvider = (new ServiceCollection())
+            .AddSingleton<IFileSystem>(fileSystem)
+            .AddSingleton<IVersionService, MockVersionService>()
+            .AddSingleton<IRepositoryAuthStore>(RepositoryAuthStore.FromEnvironmentConfig(fileSystem))
+            .AddSingleton<IRepositoryLocator>(repositoryLocator.Object)
+            .AddSingleton<IRepositoryCredentials, NPMRepositoryCredentials>()
+            .BuildServiceProvider();
+        ExecutionContext.Initialize(fileSystem);
+
+        var publishCommand = new PublishCommand(fileSystem);
+
+        // Act
+        publishCommand.Execute(emptyFolder, repositoryUrl, false, false);
+
+        // Assert
+        writer.ToString().Should().Contain($"No packages found in folder {emptyFolder}. Skipping.");
+        npmClient.Verify(x => x.PublishPackage(It.IsAny<IFileInfo>()), Times.Never);
+    }
+
+    private static byte[] CreatePackageArchiveData(string version)
+    {
+        using var zipStream = new MemoryStream();
+        using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create, leaveOpen: true))
+        {
+            using var entryStream = zipArchive.CreateEntry("package.json").Open();
+            entryStream.Write(Encoding.UTF8.GetBytes($$"""
+            {
+                "name": "Cmf.Custom.Tests",
+                "version": "{{version}}",
+                "description": "Custom Tests Package",
+                "author": "Critical Manufacturing",
+                "keywords": ["cmf-tests-package"]
+            }
+            """));
+        }
+
+        return zipStream.ToArray();
     }
 }
