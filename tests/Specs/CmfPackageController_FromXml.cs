1c008789cd3ef2c61db8480e65beb4a7e89947e3
Pedro Pinho <pedro1998pinho@gmail.com>
2026-04-24 14:23:45 +0000

    fix: extend Step class with additional properties and update XML parsing to handle new attributes

--- tests/Specs/CmfPackageController_FromXml.cs
+++ tests/Specs/CmfPackageController_FromXml.cs
@@ -70,7 +70,76 @@ public class CmfPackageController_FromXml
         pkg.Steps.Single().ImportXMLObjectPath.Should().Be("./xml");
     }
 
-      [Fact]
+    [Theory]
+    [InlineData("DeployFiles", StepType.DeployFiles)]
+    [InlineData("RunSql", StepType.RunSql)]
+    [InlineData("MasterData", StepType.MasterData)]
+    [InlineData("EnqueueXmla", StepType.EnqueueXmla)]
+    [InlineData("DatabaseAccount", StepType.DatabaseAccount)]
+    public void FromXml_ShouldParseStepType_WhenTypeAttributeIsKnown(string typeValue, StepType expectedType)
+    {
+        var xml = XDocument.Parse(
+            $"""
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="{typeValue}" contentPath="*" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().Type.Should().Be(expectedType);
+    }
+
+    [Theory]
+    [InlineData("UnknownType")]
+    [InlineData("")]
+    [InlineData(null)]
+    public void FromXml_ShouldFallbackToGenericStepType_WhenTypeAttributeIsUnknownOrMissing(string typeValue)
+    {
+        var typeAttr = typeValue != null ? $"type=\"{typeValue}\"" : "";
+        var xml = XDocument.Parse(
+            $"""
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step {typeAttr} contentPath="*" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().Type.Should().Be(StepType.Generic);
+    }
+
+    [Fact]
+    public void FromXml_ShouldFallbackToGenericStepType_WhenTypeAttributeIsMissing()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step contentPath="*" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().Type.Should().Be(StepType.Generic);
+    }
+
+    [Fact]
       public void FromXml_ShouldParseIsToForceInstall_FromManifestElement()
       {
         var xml = XDocument.Parse(
@@ -88,4 +157,241 @@ public class CmfPackageController_FromXml
 
         pkg.IsToForceInstall.Should().BeTrue();
       }
+
+    [Fact]
+    public void FromXml_ShouldParseFileAttribute_ForTransformFileStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="TransformFile" file="./config/app.config" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().File.Should().Be("./config/app.config");
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseContentAttribute_ForAutomationSyncStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="AutomationBusinessScenariosSync" content="./scenarios" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().Content.Should().Be("./scenarios");
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseUserKeyAndQuota_ForClickHouseAccountStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="ClickHouseAccount" userKey="myUser" quota="1000" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().UserKey.Should().Be("myUser");
+        pkg.Steps.Single().Quota.Should().Be("1000");
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseIdAndMessageType_ForCreateIntegrationEntriesStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="CreateIntegrationEntries" id="entry1" messageType="ImportObject" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().Id.Should().Be("entry1");
+        pkg.Steps.Single().MessageType.Should().Be(MessageType.ImportObject);
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseUseMachineNameAndAccount_ForDatabaseAccountStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="DatabaseAccount" useMachineName="true" account="sa" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().UseMachineName.Should().BeTrue();
+        pkg.Steps.Single().Account.Should().Be("sa");
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseGenericStepHandlersAndPatchAttributes()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="Generic" id="step1" onInitialize="InitHandler" onPrepare="PrepHandler" scriptHandler="ScriptH" patchId="p001" replaceTokens="true" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        var step = pkg.Steps.Single();
+        step.Id.Should().Be("step1");
+        step.OnInitialize.Should().Be("InitHandler");
+        step.OnPrepare.Should().Be("PrepHandler");
+        step.ScriptHandler.Should().Be("ScriptH");
+        step.PatchId.Should().Be("p001");
+        step.ReplaceTokens.Should().BeTrue();
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseInstallIfDbDoesntExistAttributes()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="InstallIfDbDoesntExist" sourcePackages="pkg1,pkg2" targetDb="myDb" replaceTokens="false" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        var step = pkg.Steps.Single();
+        step.SourcePackages.Should().Be("pkg1,pkg2");
+        step.TargetDb.Should().Be("myDb");
+        step.ReplaceTokens.Should().BeFalse();
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseCreateInCollection_ForMasterDataStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="MasterData" contentPath="*.xlsx" createInCollection="true" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().CreateInCollection.Should().BeTrue();
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseRunClickhouseSqlAttributes()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="RunClickhouseSql" conditionPath="./cond.sql" patchId="p001" patchDescription="init patch" replaceTokens="true" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        var step = pkg.Steps.Single();
+        step.ConditionPath.Should().Be("./cond.sql");
+        step.PatchId.Should().Be("p001");
+        step.PatchDescription.Should().Be("init patch");
+        step.ReplaceTokens.Should().BeTrue();
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseDatabaseType_ForRunSqlStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="RunSql" databaseType="Online" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().DatabaseType.Should().Be("Online");
+    }
+
+    [Fact]
+    public void FromXml_ShouldParseConfigPathAndValue_ForUpdateConfigurationStep()
+    {
+        var xml = XDocument.Parse(
+            """
+            <deploymentPackage>
+              <packageId>Cmf.Custom.Data</packageId>
+              <version>1.0.0</version>
+              <steps>
+                <step type="UpdateConfiguration" configPath="./appsettings.json" value="newValue" />
+              </steps>
+            </deploymentPackage>
+            """);
+
+        var pkg = CmfPackageController.FromXml(xml);
+
+        pkg.Steps.Should().ContainSingle();
+        pkg.Steps.Single().ConfigPath.Should().Be("./appsettings.json");
+        pkg.Steps.Single().Value.Should().Be("newValue");
+    }
 }
