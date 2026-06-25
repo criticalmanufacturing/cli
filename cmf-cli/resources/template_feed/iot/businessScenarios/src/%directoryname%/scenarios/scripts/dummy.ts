d1ac39ed5c31e11042425b3f60493a6d8ebb81a1
Roque <32566150+jrk94@users.noreply.github.com>
2026-06-25 09:31:50 +0100

    feat: add intelisense to biz scenarios scripts

---
+++ cmf-cli/resources/template_feed/iot/businessScenarios/src/%directoryname%/scenarios/scripts/dummy.ts
@@ -0,0 +1,14 @@
+﻿import { ScriptScopeBase } from "../types/globals";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class DummyWrapper extends ScriptScopeBase {
+    [key: string]: any;
+
+    private dummy() {
+        // PackagePacker: Start of Script
+        (async () => {
+        })();
+        // PackagePacker: End of Script
+    }
+}
+
