96c5c4f6ba9b735d2feb936336d63aaa30151a6e
Alberto Ponces <albertoponces@criticalmanufacturing.com>
2026-06-26 16:12:00 +0100

    chore: we only need to support these flags on the `IoT.Packages` packages

--- tests/Specs/Repositories.cs
+++ tests/Specs/Repositories.cs
@@ -93,8 +93,6 @@ public class Repositories
   ""packageType"": ""Root"",
   ""isInstallable"": true,
   ""isUniqueInstall"": false,
-  ""isToForceInstall"": true,
-  ""forceRerunAfterDatabaseRestore"": true,
   ""dependencies"": [
     {
          ""id"": ""Cmf.Custom.Business"",
@@ -679,8 +677,6 @@ public class Repositories
             <version>{packageRoot.Value}</version>
             <isInstallable>True</isInstallable>
             <isUniqueInstall>False</isUniqueInstall>
-            <isToForceInstall>True</isToForceInstall>
-            <forceRerunAfterDatabaseRestore>True</forceRerunAfterDatabaseRestore>
             <keywords>cmf-root-package</keywords>
             <dependencies>
               <dependency id=""Cmf.Environment"" version=""11.0.0"" mandatory=""false"" isIgnorable=""true"" />
