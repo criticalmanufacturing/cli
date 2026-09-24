4516ac85f02f71fe2d34369e1ea577ae4d3eb697
João Octávio Pereira <joaopereira@criticalmanufacturing.com>
2026-09-24 00:00:25 +0000

    chore: add build.json generation in HTMLCommand for MES v12 compatibility

--- cmf-cli/Commands/new/HTMLCommand.cs
+++ cmf-cli/Commands/new/HTMLCommand.cs
@@ -205,7 +205,8 @@ namespace Cmf.CLI.Commands.New
             if(mesVersion.Major >= 12)
             {
             // TODO: This should be removed after beta.2 release
-            this.fileSystem.File.WriteAllText(this.fileSystem.Path.Join(packageDir.FullName, "src/assets/build.json"), string.Empty);
+            var buildJson = "{\n  \\\"version\\\": \\\"$(Cmf.AppVersion)\\\",\n  \\\"buildDate\\\": \\\"$(Cmf.AppBuildDate)\\\"\n}";
+            this.fileSystem.File.WriteAllText(this.fileSystem.Path.Join(packageDir.FullName, "src/assets/build.json"), buildJson);
             var maints = 
 @"/// <reference types=""@angular/localize"" />
 
